49649202d3779e94fb7c3fd9ebe0f5fb8d83a65a
borchowiec <borchowiecpatryk@gmail.com>
2020-05-19 11:49:53 +0200

    Init

---
+++ AccessLayer/rest-api-access/implementation/src/main/java/com/example/configuration/RestApiSpringConfiguration.java
@@ -0,0 +1,25 @@
+package com.example.configuration;
+
+import com.jlupin.impl.client.util.JLupinClientUtil;
+import com.jlupin.interfaces.client.delegator.JLupinDelegator;
+import com.jlupin.interfaces.common.enums.PortType;
+import com.jlupin.servlet.monitor.annotation.EnableJLupinSpringBootServletMonitor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.example")
+@EnableJLupinSpringBootServletMonitor
+public class RestApiSpringConfiguration {
+    @Bean
+    public JLupinDelegator getJLupinDelegator() {
+        return JLupinClientUtil.generateInnerMicroserviceLoadBalancerDelegator(PortType.JLRMC);
+    }
+
+    // @Bean(name = "exampleService")
+    // public ExampleService getExampleService() {
+    //     return JLupinClientUtil.generateRemote(getJLupinDelegator(), "example-microservice", ExampleService.class);
+    // }
+}
+
---
+++ integration-test/src/test/java/com/example/data/layer/UserStorageTest.java
@@ -0,0 +1,12 @@
+package com.example.data.layer;
+
+import com.example.base.BaseTest;
+
+public class UserStorageTest extends BaseTest {
+    // Example test
+    // @Test
+    // public void exampleTest() {
+    //     ExampleService service = JLupinClientUtil.generateRemote(getJLupinDelegator(), "user-storage", ExampleService.class);
+    //     assertEquals("2 + 2 must be 4", new Integer(4), service.add(2, 2));
+    // }
+}
